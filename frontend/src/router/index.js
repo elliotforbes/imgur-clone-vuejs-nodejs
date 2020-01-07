1f180486dd3e7f598edd91998726eb6926d1fc01
Elliot Forbes <emforce@live.com>
2020-01-07 21:27:02 +0000

    Part 5 under construction

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,15 +5,37 @@ import Login from './../components/Login.vue'
 import Register from './../components/Register.vue'
 import Single from './../components/Single.vue'
 import Profile from './../components/Profile.vue'
-
+import cognitoAuth from '@/cognito'
 Vue.use(Router)
+
+function requireAuth (to, from, next) {
+  cognitoAuth.isAuthenticated((err, loggedIn) => {
+    if (err) return next()
+    if (!loggedIn) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  })
+}
+
+function logout(to, from, next) {
+  cognitoAuth.logout()
+  next('/')
+}
+
 
 export default new Router({
     routes: [
       { path: '/', component: HomePage },
       { path: '/login', component: Login },
       { path: '/register', component: Register },
-      { path: '/Profile', component: Profile },
+      { path: '/Profile', component: Profile, beforeEnter: requireAuth },
+      { path: '/upload', component: Upload, beforeEnter: requireAuth },
       { path: '/:id', component: Single },
+      { path: '/logout', beforeEnter: logout }
     ]
 })
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,21 @@ resource "aws_s3_bucket" "bucket" {
     }
 }
 
+resource "aws_cognito_user_pool" "imgur_clone_pool" {
+    name = "imgurclonepool"   
+}
+
+resource "aws_cognito_identity_pool" "imgur_clone_identity_pool" {
+    
+}
+
+
+output "imgur_clone_client_id" {
+  value = aws_cognito_user_pool.imgur_clone_pool.id
+}
+
+output "imgur_clone_client_arn" {
+  value = aws_cognito_user_pool.imgur_clone_pool.arn
+}
+
+output @
