74bbfa4a7990acb67c804b986de7f1a40704652c
Elliot Forbes <emforce@live.com>
2020-01-29 20:22:25 +0000

    Working now with cors added to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,15 @@ provider "aws" {
 resource "aws_s3_bucket" "bucket" {
     bucket = "dev-imgur-clone-bucket-test"
       
+    cors_rule {
+      allowed_headers = ["*"]
+      allowed_methods = ["PUT", "POST"]
+      allowed_origins = ["*"]
+      expose_headers  = ["x-amz-server-side-encryption", "x-amz-request-id", "x-amz-id-2"]
+      max_age_seconds = 3000
+    }
+
+
     tags = {
         Name = "Dev Imgur Clone Bucket"
         Environment = "Dev"
