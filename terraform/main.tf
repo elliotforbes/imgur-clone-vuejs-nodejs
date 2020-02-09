9accf60a3ad8c146b7419161c6a8e67852f44b2c
Elliot Forbes <emforce@live.com>
2020-02-09 10:45:22 +0000

    Adding starting point for serverless deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,7 @@ output "ClientId" {
 output "frontend-bucket" {
     value = "${aws_s3_bucket.frontend_bucket.bucket}"
 }
+
+output "app-path" {
+    value = "${aws_s3_bucket.frontend_bucket.website_endpoint}"
+}
